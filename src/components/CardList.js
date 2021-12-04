0c67f8445d93e07fb6c0a5307626d9ee6eef7998
Angelo <angelo.adduci1991@gmail.com>
2021-12-04 20:21:44 +0100

    Rename cats var

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Card from './Card'
 
-const CardList = ( {cats} ) => {
+const CardList = ( {catsList} ) => {
     return (
         <section className="flex justify-center items-center flex-wrap ma3 pv3">
-            { cats.map( (catsItem, index) => {
+            { catsList.map( (catsItem, index) => {
                 return <Card
                     key={index}
                     id={catsItem.id}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,52 +1,52 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import CardList from '../components/CardList'
 import Header from '../components/Header'
 import Scroll from '../components/Scroll'
 import './App.css'
 import ErrorBoundry  from '../components/ErrorBoundry'
-import { setSearchField } from '../actions/actions'
+import { setSearchField, setCatsList } from '../actions/actions'
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchField,
+        catsList: state.catsList
     }
 }
 
 const mapDispatchToProps = disptch => {
     return {
         onSearchChange: (event) => disptch(setSearchField(event.target.value)),
+        filterCats: (cats) => disptch(setCatsList(cats))
     }
 }
 
 
 const App = (AppProps) => {
-    const [ cats, setCats ] = useState([])
+    const [ catsList, setCatsList] = useState([])
 
-    const { searchField, onSearchChange} = AppProps
+    const { onSearchChange, searchField } = AppProps
     
     useEffect(()  => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => {
-            return setCats(users)
-        })
+        .then(catsList => setCatsList(catsList))
         .catch(e => console.log(e))
     })
 
-    const filteredCats = cats.filter( cat => {
+    const filterCatsList = catsList.filter( cat => {
         return cat.name.toLowerCase().includes(searchField.toLowerCase())
     })
 
     return (
-        (!cats.length) ?
+        (!catsList.length) ?
             <h1 className="heading-style-custom tc">Loading</h1> :
             (
                 <>
                     <Header onSearchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
-                            <CardList cats={ filteredCats } />
+                            <CardList catsList={filterCatsList} />
                         </ErrorBoundry>
                     </Scroll>
                 </>
